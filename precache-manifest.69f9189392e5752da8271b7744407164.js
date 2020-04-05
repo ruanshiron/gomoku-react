5098afe688b0e3548c93588dc714216e5dab1ff4
Vinh <theten12@gmail.com>
2020-04-06 03:15:10 +0700

    Updates

---
+++ precache-manifest.69f9189392e5752da8271b7744407164.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fdef5b64e17ea092bb7c5aed4eb9333",
+    "url": "/gomoku-react/index.html"
+  },
+  {
+    "revision": "8d57c4a7d85b77db14ce",
+    "url": "/gomoku-react/static/css/main.7bfd2d39.chunk.css"
+  },
+  {
+    "revision": "b2be174359f9b1f0c98b",
+    "url": "/gomoku-react/static/js/2.8b79a195.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gomoku-react/static/js/2.8b79a195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d57c4a7d85b77db14ce",
+    "url": "/gomoku-react/static/js/main.8e3d686f.chunk.js"
+  },
+  {
+    "revision": "8d4f3e67032634106ee2",
+    "url": "/gomoku-react/static/js/runtime-main.a99e8021.js"
+  }
+]);
