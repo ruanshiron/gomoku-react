c3eed300be52f60ede83da803416438ea92fde9b
hungking98 <hungking98@gmail.com>
2020-04-06 01:54:01 +0700

    Board component

--- src/component/Board.js
+++ src/component/Board.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import './Board.css'
+import Square from './Square'
 
-function Board(props) {
+function Board({current, onChange, winner}) {
   return (
-    <div>
-      
+    <div className="w3-justify">
+      <div className="board-container">
+        <div className="board">
+          {
+            current.board.map((x,i) => (
+              <div 
+                key={i} 
+                className="board-row"
+              >
+                {
+                  x.map((sx,j) => (
+                    <Square
+                      key={j}
+                      mark={sx}
+                      onClick={(e) => onChange(i, j)}
+                      disabled={winner!=null? true: false}
+                    />
+                  ))
+                }
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </div>
   )
 }
