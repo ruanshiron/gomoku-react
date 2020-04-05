4a761e8a96693facd0d148e288a6bd72d19c8b0f
Bunbokun <PineCXp@gmail.com>
2020-04-05 19:55:27 +0000

    them square va stats

--- src/component/Stats.js
+++ src/component/Stats.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
-function Stats(props) {
+function Stats({data}) {
   return (
-    <div>
-      
+    <div className="w3-card-0">
+      {
+        data.length > 0 &&
+        <>
+          <header className="w3-container w3-light-grey">
+            <h4>統計</h4>
+          </header>
+          <ul className="w3-ul ">
+            {
+              data.map((o, i) => 
+              <li key={i}>#{i+1}. <b>{o? "O" : "X"}</b>の勝ちです</li>
+              )
+            }
+          </ul>
+        </>
+      }
     </div>
   )
 }
-
-export default Stats
